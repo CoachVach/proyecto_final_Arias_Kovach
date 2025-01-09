5ec7cbe2ec94f245503bc29087dcf40781e018e2
Erik Kovach <kovacherik@gmail.com>
2025-01-09 15:21:41 -0300

    refactor: dashboard to Home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
 import UserPage from './components/UserPage';
-
-const Dashboard = () => <h1>Bienvenido al Dashboard</h1>;
 
 const App = () => {
   return (
@@ -18,7 +17,7 @@ const App = () => {
           path="/dashboard"
           element={
             <ProtectedRoute>
-              <Dashboard />
+              <Home />
             </ProtectedRoute>
           }
         />
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,3 @@
+const Home = () => <h1>Bienvenido al Dashboard</h1>;
+
+export default Home;
