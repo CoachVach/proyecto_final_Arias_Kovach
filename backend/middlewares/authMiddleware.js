dc2ae7f49cd47b508edbeb96ff47e09b269454e2
Federico Arias <ariasfede596@gmail.com>
2025-01-15 18:16:46 -0300

    Update en authMiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY || 'tu_clave_secreta';
+const Profesor = require('../models/profesor');
 
 // Middleware para validar el token
-const verifyToken = (req, res, next) => {
+const verifyToken = async (req, res, next) => {
   const token = req.headers['authorization'];
   if (!token) {
     return res.status(403).json({ message: 'Token no proporcionado' });
@@ -12,13 +13,18 @@ const verifyToken = (req, res, next) => {
   const tokenWithoutBearer = token.split(' ')[1];
 
   // Verify the token
-  jwt.verify(tokenWithoutBearer, SECRET_KEY, (err, decoded) => {
+  jwt.verify(tokenWithoutBearer, SECRET_KEY, async (err, decoded) => {
     if (err) {
       return res.status(401).json({ message: 'Token inválido' });
     }
 
+    const profesor = await Profesor.findOne({ where: { email: decoded.email } });
+    if (!profesor) {
+        return res.status(404).json({ error: 'Profesor no encontrado' });
+    }
+
     // Attach decoded information to the request (you can also add email or id)
-    req.profesorEmail = decoded.email;
+    req.profesor = profesor;
     next();
   });
 };
