969cc5163b756003954c4cb0251c5dce1d4608ee
Erik Kovach <kovacherik@gmail.com>
2025-01-27 20:34:33 -0300

    Front

--- frontend/src/components/ModalWrapper.js
+++ frontend/src/components/ModalWrapper.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Modal from './common/Modal';
 import QRScanner from './QRScanner';
 import FileModal from './FileModal';
-import AlumnoMesaUpdate from './AlumnoMesaUpdate';
+import AlumnoMesaUpdate from './alumnoMesaUpdate';
 
 const ModalWrapper = ({ modalState, closeModal, handleQRCodeScanned, alumnos, mesa }) => {
   if (!modalState.type) return null;
