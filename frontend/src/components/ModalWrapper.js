04df7419f555dec1b61f2b3dfddadc8cde91bf4a
Erik Kovach <kovacherik@gmail.com>
2025-02-13 11:36:04 -0300

    cositas

--- frontend/src/components/ModalWrapper.js
+++ frontend/src/components/ModalWrapper.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Modal from './common/Modal';
 import FileModal from './FileModal';
-import AlumnoMesaUpdate from './AlumnoMesaUpdate';
+import AlumnoMesaUpdate from './alumnoMesaUpdate';
 import QRScanner from './QRScanner';
 import '../styles/components/QRScanner.css';
 
